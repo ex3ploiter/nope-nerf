aa45954a896196f1c09cb94f1625f98bce025a4e
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2023-12-31 11:25:47 +0330

    x

--- scene/__init__.py
+++ scene/__init__.py
@@ -40,11 +40,11 @@ class Scene:
         self.train_cameras = {}
         self.test_cameras = {}
 
-        if os.path.exists(os.path.join(args.source_path, "sparse")):
-            scene_info = sceneLoadTypeCallbacks["Colmap"](args.source_path, args.images, args.eval)
-        elif os.path.exists(os.path.join(args.source_path, "transforms_train.json")):
+        if os.path.exists(os.path.join('/content/gaussian-splatting', "sparse")):
+            scene_info = sceneLoadTypeCallbacks["Colmap"]('/content/gaussian-splatting', args.images, args.eval)
+        elif os.path.exists(os.path.join('/content/gaussian-splatting', "transforms_train.json")):
             print("Found transforms_train.json file, assuming Blender data set!")
-            scene_info = sceneLoadTypeCallbacks["Blender"](args.source_path, args.white_background, args.eval)
+            scene_info = sceneLoadTypeCallbacks["Blender"]('/content/gaussian-splatting', args.white_background, args.eval)
         else:
             assert False, "Could not recognize scene type!"
 
