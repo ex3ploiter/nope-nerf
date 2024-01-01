73787be7a7e9291a650bcebbf002dfcee1995cf6
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2024-01-01 11:30:51 +0330

    x

--- model/training.py
+++ model/training.py
@@ -84,25 +84,14 @@ class Trainer(object):
         '''
         # self.model.train()
         self.optimizer.zero_grad()
-        if self.pose_param_net:
-           self.pose_param_net.train()
-           self.optimizer_pose.zero_grad()
-        if self.focal_net:
-            self.focal_net.train()
-            self.optimizer_focal.zero_grad()
-        if self.distortion_net:
-            self.distortion_net.train()
-            self.optimizer_distortion.zero_grad()
+      
         loss_dict = self.compute_loss(data, it=it, epoch=epoch, scheduling_start=scheduling_start, out_render_path=render_path,pipe=pipe,bg=bg)
         loss = loss_dict['loss']
         loss.backward()
         self.optimizer.step()
-        if self.optimizer_pose:
-            self.optimizer_pose.step()
-        if self.optimizer_focal:
-            self.optimizer_focal.step()
-        if self.optimizer_distortion:
-            self.optimizer_distortion.step()
+       
+
+       
         return loss_dict
 
     
