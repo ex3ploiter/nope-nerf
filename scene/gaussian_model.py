faf7f0b086068b57158eaae6dcd21a38d58dad16
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2024-01-01 10:47:41 +0330

    x

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -124,11 +124,10 @@ class GaussianModel:
     def create_from_pcd(self, pcd : BasicPointCloud, spatial_lr_scale : float):
         self.spatial_lr_scale = spatial_lr_scale
         fused_point_cloud = torch.tensor(np.asarray(pcd.points)).float().cuda()
-        
         fused_color = RGB2SH(torch.tensor(np.asarray(pcd.colors)).float().cuda())
-        features = torch.zeros((fused_color.shape[0],fused_color.shape[0], 3, (self.max_sh_degree + 1) ** 2)).float().cuda()
-        features[:,:, :3, 0 ] = fused_color
-        features[:,:, 3:, 1:] = 0.0
+        features = torch.zeros((fused_color.shape[0], 3, (self.max_sh_degree + 1) ** 2)).float().cuda()
+        features[:, :3, 0 ] = fused_color
+        features[:, 3:, 1:] = 0.0
 
         print("Number of points at initialisation : ", fused_point_cloud.shape[0])
 
