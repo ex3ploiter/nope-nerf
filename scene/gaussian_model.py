2dd08fdc0cfc82c6e21adbf8641ca9ace70f295a
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2024-01-01 10:35:41 +0330

    x

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -126,7 +126,7 @@ class GaussianModel:
         fused_point_cloud = torch.tensor(np.asarray(pcd.points)).float().cuda()
         
         fused_color = RGB2SH(torch.tensor(np.asarray(pcd.colors)).float().cuda())
-        features = torch.zeros((fused_color.shape[0], 3, (self.max_sh_degree + 1) ** 2)).float().cuda()
+        features = torch.zeros((fused_color.shape[0],fused_color.shape[0], 3, (self.max_sh_degree + 1) ** 2)).float().cuda()
         features[:,:, :3, 0 ] = fused_color
         features[:,:, 3:, 1:] = 0.0
 
