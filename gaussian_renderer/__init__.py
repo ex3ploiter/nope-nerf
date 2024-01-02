722eec263f58284d66fa7e79b82a6dc4dab513e6
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2024-01-02 20:02:37 +0330

    x

--- gaussian_renderer/__init__.py
+++ gaussian_renderer/__init__.py
@@ -142,6 +142,7 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
 
 
 def render_transform(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, scaling_modifier = 1.0, override_color = None,idx=None,rot=None,trans=None):
+
     """
     Render the scene. 
     
@@ -149,13 +150,12 @@ def render_transform(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torc
     """
  
     # Create zero tensor. We will use it to make pytorch return gradients of the 2D (screen-space) means
-    
-    screenspace_points = torch.zeros_like(pc.get_xyz_render(idx), dtype=pc.get_xyz_render(idx).dtype, requires_grad=True, device="cuda") + 0
+    screenspace_points = torch.zeros_like(pc.get_xyz, dtype=pc.get_xyz.dtype, requires_grad=True, device="cuda") + 0
     try:
         screenspace_points.retain_grad()
     except:
         pass
-    
+
     # Set up rasterization configuration
     tanfovx = math.tan(viewpoint_camera.FoVx * 0.5)
     tanfovy = math.tan(viewpoint_camera.FoVy * 0.5)
@@ -177,10 +177,9 @@ def render_transform(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torc
 
     rasterizer = GaussianRasterizer(raster_settings=raster_settings)
 
-    means3D = pc.get_xyz_render(idx)
-    means3D=rotate_vector_with_quaternion(means3D,trans,rot)
+    means3D = pc.get_xyz
     means2D = screenspace_points
-    opacity = pc.get_opacity_render(idx)
+    opacity = pc.get_opacity
 
     # If precomputed 3d covariance is provided, use it. If not, then it will be computed from
     # scaling / rotation by the rasterizer.
@@ -188,15 +187,10 @@ def render_transform(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torc
     rotations = None
     cov3D_precomp = None
     if pipe.compute_cov3D_python:
-        cov3D_precomp = pc.get_covariance_render(scaling_modifier,idx)
-        
+        cov3D_precomp = pc.get_covariance(scaling_modifier)
     else:
-        scales = pc.get_scaling_render(idx)
-        scales=rotate_vector_with_quaternion(scales,trans,rot)
-        
-        rotations = pc.get_rotation_render(idx)
-
-        
+        scales = pc.get_scaling
+        rotations = pc.get_rotation
 
     # If precomputed colors are provided, use them. Otherwise, if it is desired to precompute colors
     # from SHs in Python, do it. If not, then SH -> RGB conversion will be done by rasterizer.
@@ -204,25 +198,27 @@ def render_transform(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torc
     colors_precomp = None
     if override_color is None:
         if pipe.convert_SHs_python:
-            shs_view = pc.get_features_render(idx).transpose(1, 2).view(-1, 3, (pc.max_sh_degree+1)**2)
-            dir_pp = (rotate_vector_with_quaternion(pc.get_xyz_render(idx),trans,rot) - viewpoint_camera.camera_center.repeat(pc.get_features_render(idx).shape[0], 1))
+            shs_view = pc.get_features.transpose(1, 2).view(-1, 3, (pc.max_sh_degree+1)**2)
+            dir_pp = (pc.get_xyz - viewpoint_camera.camera_center.repeat(pc.get_features.shape[0], 1))
             dir_pp_normalized = dir_pp/dir_pp.norm(dim=1, keepdim=True)
             sh2rgb = eval_sh(pc.active_sh_degree, shs_view, dir_pp_normalized)
             colors_precomp = torch.clamp_min(sh2rgb + 0.5, 0.0)
         else:
-            shs = pc.get_features_render(idx)
+            shs = pc.get_features
     else:
         colors_precomp = override_color
 
     # Rasterize visible Gaussians to image, obtain their radii (on screen). 
+    print("means2D : ",means2D.shape)
+    
     rendered_image, radii = rasterizer(
-        means3D = means3D,
+        means3D = rotate_vector_with_quaternion(means3D,trans,rot),
         means2D = means2D,
         shs = shs,
         colors_precomp = colors_precomp,
         opacities = opacity,
-        scales = scales,
-        rotations = rotations,
+        scales = rotate_vector_with_quaternion(scales,trans,rot),
+        rotations = rotate_vector_with_quaternion(rotations,trans,rot),
         cov3D_precomp = cov3D_precomp)
 
     # Those Gaussians that were frustum culled or had a radius of 0 were not visible.
@@ -231,6 +227,3 @@ def render_transform(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torc
             "viewspace_points": screenspace_points,
             "visibility_filter" : radii > 0,
             "radii": radii}
-    
-    
-    
