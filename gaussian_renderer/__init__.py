ecf091f76cb95b809bf30bac36101b941a8b148f
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2024-01-02 20:15:47 +0330

    x

--- gaussian_renderer/__init__.py
+++ gaussian_renderer/__init__.py
@@ -34,6 +34,7 @@ def rotate_vector_with_quaternion(vector, trans,quaternion):
     
     # vector_quaternion = torch.cat((torch.tensor([0.],device='cuda',requires_grad=True), vector))
     vector_quaternion = torch.cat((torch.zeros((vector.shape[0],1),device='cuda',requires_grad=True), vector),dim=1)
+    print("vector_quaternion ",vector_quaternion.requires_grad)
 
     # Apply quaternion multiplication to rotate the vector
     rotated_vector_quaternion = quaternion_multiply(
